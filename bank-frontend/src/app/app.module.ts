cf9ac190f006552a3bceb337a1df74c0b5da89f0
Kristina Stojic <stojic.kris@gmail.com>
2022-11-23 21:46:50 +0100

    feat: front login

--- bank-frontend/src/app/app.module.ts
+++ bank-frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TokenInterceptor } from './interceptor/token-interceptor';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,19 +7,32 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { CardInfoComponent } from './components/card-info/card-info.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { PersonalInfoComponent } from './components/personal-info/personal-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignUpComponent,
-    CardInfoComponent
+    CardInfoComponent,
+    PersonalInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }, 
+  ],  bootstrap: [AppComponent]
 })
 export class AppModule { }
